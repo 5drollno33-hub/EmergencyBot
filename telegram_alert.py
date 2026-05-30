7d27884cedb56e69c7c66ef7002b65a2f00314fc
Siddharth <5drollno33@gmail.com>
2026-05-30 12:08:39 +0530

    Use environment variables

--- telegram_alert.py
+++ telegram_alert.py
@@ -1,16 +1,5 @@
+import os
 import requests
 
-BOT_TOKEN = "8751910148:AAEVfOuXy4kgaQxnd2G9PHa5UMhDErD8Tz8"
-CHAT_ID = "7798249693"
-
-def send_alert(message):
-
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
-    requests.post(
-        url,
-        data={
-            "chat_id": CHAT_ID,
-            "text": message
-        }
-    )
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
